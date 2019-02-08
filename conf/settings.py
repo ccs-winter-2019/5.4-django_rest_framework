df621cf8d0c50b9da220fc072cb56039dbaf4f5d
Dan Dietz <danddietz@gmail.com>
2019-02-08 13:54:13 -0400

    Updating static config for heroku

--- conf/settings.py
+++ conf/settings.py
@@ -142,6 +142,6 @@ STATICFILES_FINDERS = [
 ]
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
